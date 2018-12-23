4f7f86b18dca06934d177395c2593769f9e7a1c6
NedaK <nedak26@gmail.com>
2018-12-23 15:25:28 -0800

    added images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,8 +19,8 @@ var pickedWord = words[Math.floor(Math.random() * words.length)];
 console.log(pickedWord);
 
 // Set number of guesses to the amount of letters in the selected word
-remainingGuesses = pickedWord.length;
-console.log(remainingGuesses);
+remainingGuesses = pickedWord.length + 2;
+userTextGuess.textContent = remainingGuesses;
 
 
 // create an array with "-" for the number of characters in the picked word
@@ -32,37 +32,113 @@ var answerArray = [];
 // document.write("<h1>The word you are guessing is: " + answerArray.join(" ")+ "</h1>");
 userTextWord.textContent = answerArray.join(" ");
 
-// When a player pushes a key, store that in a variable, and check if it is in the pickedWord
-document.onkeyup = function(event){
-    var letter = event.key;
+    
+function resetGame(){
+    
+    lettersGuessed = [];
+    pickedWord = words[Math.floor(Math.random() * words.length)];
 
-    lettersGuessed.push(letter);
-    remainingGuesses--;
+    console.log(pickedWord);
+
+    remainingGuesses = pickedWord.length + 2;
+    userTextGuess.textContent = remainingGuesses;
+    userTextLetters.textContent = lettersGuessed;
+
+    answerArray = [];
+    for (var i = 0; i < pickedWord.length; i++) {
+        answerArray[i] = "_";
+    }
+    userTextWord.textContent = answerArray.join(" ");
+    }
+
+
+    // When a player pushes a key, store that in a variable, and check if it is in the pickedWord
+    document.onkeyup = function(event){
+        
+        var letter = event.key;
+        
+        
+        lettersGuessed.push(letter);
+        remainingGuesses--;
+        
+        
 
         for(var l = 0; l < pickedWord.length; l++){
             
             if(letter === pickedWord[l]){
                 answerArray.splice(l, 1, letter);
+                
+                userTextWord.textContent = answerArray.join(" ");
                 console.log(answerArray.join(" "));
+            
             
             } 
             
+                // let unique = [...new Set(lettersGuessed)];
+                
+                // console.log(unique);
+                // console.log(lettersGuessed);
+                // console.log(remainingGuesses);
+                
+                
+
+                // userTextWord.textContent = answerArray.join(" ");
+                // userTextGuess.textContent = remainingGuesses;
+                // userTextLetters.textContent = unique.join(" ");
         }
+
+                let unique = [...new Set(lettersGuessed)];
+                
+                // console.log(unique);
+                // console.log(lettersGuessed);
+                // console.log(remainingGuesses);
+                
+
+                userTextWord.textContent = answerArray.join(" ");
+                userTextGuess.textContent = remainingGuesses;
+                userTextLetters.textContent = unique.join(" ");
+
+        if (remainingGuesses === 0 && answerArray.includes("_") === true){
+            // console.log("YOU LOST! ---- Starting new game");
+             alert("You Lost!! You were trying to guess " + pickedWord);
+           // document.write("<h1>YOU LOST!!! You were trying to guess </h1>" + "<h1>" pickedWord "</h1>");
+           
+            setTimeout(resetGame, 2000);
+            //resetGame();
+        }
+
+        else if(answerArray.includes("_") === false){
+            
+            // let unique = [...new Set(lettersGuessed)];
+            userTextWord.textContent = answerArray.join(" ");
+            // userTextGuess.textContent = remainingGuesses;
+            //  userTextLetters.textContent = unique.join(" ");
+            alert("Winner! --- Starting new game!");
+            //alert("You Won! The word is " + pickedWord);
+
+            setTimeout(resetGame, 2000);
+            // resetGame();
+        }
+            
+        
         //make a unique array of guessed letters to display
-        let unique = [...new Set(lettersGuessed)];
+        // let unique = [...new Set(lettersGuessed)];
         
-        // console.log(unique);
-        // console.log(lettersGuessed);
-        // console.log(remainingGuesses);
+        // // console.log(unique);
+        // // console.log(lettersGuessed);
+        // // console.log(remainingGuesses);
         
         
 
-        userTextWord.textContent = answerArray.join(" ");
-        userTextGuess.textContent = remainingGuesses;
-        userTextLetters.textContent = unique.join(" ");
+        // userTextWord.textContent = answerArray.join(" ");
+        // userTextGuess.textContent = remainingGuesses;
+        // userTextLetters.textContent = unique.join(" ");
+
+    }
 
 
 
-}
+
+
 
 
