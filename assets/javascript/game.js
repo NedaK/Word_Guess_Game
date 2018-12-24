51840b0dee033e9fff805f26d32021bb63d575a0
NedaK <nedak26@gmail.com>
2018-12-24 11:41:42 -0800

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var lettersGuessed = [];
 var userTextWord = document.getElementById("word");
 var userTextGuess = document.getElementById("guessLeft");
 var userTextLetters = document.getElementById("letters");
+var hiddenText = document.getElementById("hidden");
         
 
 
@@ -18,8 +19,8 @@ var words = ["wesley", "buttercup", "inigo", "inconcievable", "giant", "fezzik",
 // randomly pick an index from the word array and set to the variable pickedWord
 
 
-  var pickedWord = words[Math.floor(Math.random() * words.length)];
-console.log(pickedWord);
+var pickedWord = words[Math.floor(Math.random() * words.length)];
+//console.log(pickedWord);
 
 // Set number of guesses to the amount of letters in the selected word
 remainingGuesses = pickedWord.length + 2;
@@ -32,7 +33,7 @@ var answerArray = [];
  answerArray[i] = "_";
 }
 // display word to be guessed with --- on the screen
-//  document.write("<h1>The word you are guessing is: " + answerArray.join(" ")+ "</h1>");
+
 userTextWord.textContent = answerArray.join(" ");
 
 
@@ -44,7 +45,7 @@ function resetGame(){
     lettersGuessed = [];
     pickedWord = words[Math.floor(Math.random() * words.length)];
 
-    console.log(pickedWord);
+    //console.log(pickedWord);
 
     remainingGuesses = pickedWord.length + 2;
     userTextGuess.textContent = remainingGuesses;
@@ -55,6 +56,8 @@ function resetGame(){
         answerArray[i] = "_";
     }
     userTextWord.textContent = answerArray.join(" ");
+    hiddenText.textContent = "";
+
     }
 
 
@@ -63,18 +66,14 @@ function resetGame(){
         
         var letter = event.key;
         
-        
-        lettersGuessed.push(letter);
-        //remainingGuesses--;
-        // for(var a =0; a<answerArray.length; a++){
-            if (answerArray.indexOf(letter) === -1){
-             remainingGuesses--;
+    //decrement remaining guesses only if new letter is guessed
 
-          }
-        // }
-        
-        
-        
+            if (lettersGuessed.indexOf(letter) === -1){
+                lettersGuessed.push(letter);
+                remainingGuesses--;
+
+          }   
+    //check if guessed letter is in selected word.  Display on screen updated results.
 
         for(var l = 0; l < pickedWord.length; l++){
             
@@ -86,58 +85,41 @@ function resetGame(){
             
             }          
         }
-
-                let unique = [...new Set(lettersGuessed)];
-                
                 userTextWord.textContent = answerArray.join(" ");
                 userTextGuess.textContent = remainingGuesses;
+
+                let unique = [...new Set(lettersGuessed)];
                 userTextLetters.textContent = unique.join(" ");
+
+    //Lose game if no guesses left and answerArray still not full of letters
 
         if (remainingGuesses === 0 && answerArray.includes("_") === true){
             
 
-            var audio = new Audio('assets/images/inconceivable.mp3');
+            var audio = new Audio('assets/images/boo.mp3');
             audio.play();
-             //alert("You Lost!! You were trying to guess " + pickedWord);
-            // document.write("<h1>YOU LOST!!! You were trying to guess the word " + pickedWord  + "</h1>");
             
-            setTimeout(resetGame, 2000);
-            //resetGame();
+            hiddenText.innerHTML = "You Lost! <br> The word you were trying to guess was: "+ pickedWord;
+           
+            setTimeout(resetGame, 7000);
+           
         }
-
+    //win game if answerArray no longer has blanks
         else if(answerArray.includes("_") === false){
             
-            // let unique = [...new Set(lettersGuessed)];
+            
             userTextWord.textContent = answerArray.join(" ");
-            // userTextGuess.textContent = remainingGuesses;
-            //  userTextLetters.textContent = unique.join(" ");
+            
 
-            var audio = new Audio('assets/images/killed.mp3');
+            var audio = new Audio('assets/images/inconceivable.mp3');
             audio.play();
             
-            //alert("You Won! The word is " + pickedWord);
-
-
+            hiddenText.innerHTML = "You Won! <br> ---New Game Starting---";
             
-            setTimeout(alert("Winner! --- Starting new game!"), 15000);
-
              setTimeout(resetGame, 5000);
-            // resetGame();
+            
         }
             
-        
-        //make a unique array of guessed letters to display
-        // let unique = [...new Set(lettersGuessed)];
-        
-        // // console.log(unique);
-        // // console.log(lettersGuessed);
-        // // console.log(remainingGuesses);
-        
-        
-
-        // userTextWord.textContent = answerArray.join(" ");
-        // userTextGuess.textContent = remainingGuesses;
-        // userTextLetters.textContent = unique.join(" ");
 
     }
 
